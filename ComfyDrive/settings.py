d9055d3feb08929a36a274aaa5852a5d295c448f
maximajin <maximajin.work@gmail.com>
2025-06-14 00:55:26 +0300

    fixed stucture

--- ComfyDrive/settings.py
+++ ComfyDrive/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'unsafe-default')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['.onrender.com']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
 # Application definition
 
